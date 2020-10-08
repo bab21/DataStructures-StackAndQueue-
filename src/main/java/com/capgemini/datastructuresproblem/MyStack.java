2b140f43d3e1c2ac72622c758b7db108206ed969
bab21 <2016061@iiitdmj.ac.in>
2020-10-08 14:34:07 +0530

    pushing gitignore

--- src/main/java/com/capgemini/datastructuresproblem/MyStack.java
+++ src/main/java/com/capgemini/datastructuresproblem/MyStack.java
@@ -1,11 +1,13 @@
 package com.capgemini.datastructuresproblem;
 
 public class MyStack <K>{
+	
 	public MyLinkedList<K> myLinkedList;
 	
 	public MyStack(){
 		this.myLinkedList=new MyLinkedList<>();
 	}
+	//...
 	public void push(INode myNode) {
 		myLinkedList.add(myNode);
 	}
